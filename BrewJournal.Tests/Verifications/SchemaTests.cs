11a478750a5ef61979c404ac43a6e5935679e61c
dionfoster <dion.foster@gmail.com>
2016-10-08 09:46:52 +0800

    - Add schema verification tests.

---
+++ BrewJournal.Tests/Verifications/SchemaTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.Infrastructure;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using BrewJournal.Tests.Testability;
+using Xunit;
+
+namespace BrewJournal.Tests.Verifications
+{
+    public class SchemaTests : DatabaseFixture
+    {
+        [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        public void VerifySchemaChange()
+        {
+            var sqlScript = ((IObjectContextAdapter)WorkDbContext).ObjectContext.CreateDatabaseScript();
+
+            Approvals.Verify(sqlScript);
+        }
+    }
+}
